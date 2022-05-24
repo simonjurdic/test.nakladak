13ee001cae0878cabc927d0ebdfbcd19d43a2738
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2022-05-24 09:03:47 +0200

    HOTOVO

--- test.nakladak/MainWindow.xaml.cs
+++ test.nakladak/MainWindow.xaml.cs
@@ -20,25 +20,25 @@ namespace test.nakladak
     /// </summary>
     public partial class MainWindow : Window
     {
-        Nakladak Mercedes;
-        Nakladak Renault;
+        Nakladak.Nakladak Mercedes;
+        Nakladak.Nakladak Renault;
         public MainWindow()
         {
             InitializeComponent();
-            Mercedes = new Nakladak();
+            Mercedes = new Nakladak.Nakladak();
             Mercedes.Jmeno = "Mercedes";
             Zobraz(Mercedes, txtBox1);
 
-            Renault = new Nakladak();
+            Renault = new Nakladak.Nakladak();
             Renault.Jmeno = "Renault";
             Zobraz(Renault, txtBox2);
         }
-        public void Zobraz(Nakladak nakladak, TextBox textBox)
+        public void Zobraz(Nakladak.Nakladak nakladak, TextBox textBox)
         {
             textBox.Text = nakladak.Jmeno + " \n";
             textBox.Text += "Nosnost v kg: " + nakladak.Nosnost.ToString() + "\n";
             textBox.Text += "Palivo v nadrzi: " + nakladak.PalivoVNadrzi.ToString() + "\n";
-            textBox.Text += "Spotreba Paliva l/km: " + nakladak.SpotrebaPaliva.ToString() + "\n";
+            textBox.Text += "Spotreba Paliva l/100 km: " + nakladak.SpotrebaPaliva.ToString() + "\n";
             textBox.Text += "Celkova UjetaVzdalenost: " + nakladak.CelkovaUjetaVzdalenost.ToString() + "\n";
             textBox.Text += "Naklad: " + nakladak.Naklad.ToString() + "\n";
         }
--- test.nakladak/Nakladak.cs
+++ test.nakladak/Nakladak.cs
@@ -2,23 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace test.nakladak
+namespace test.Nakladak
 {
     public class Nakladak
     {
-        public int nosnost;
-        public int palivovnadrzi;
-        public int spotrebapaliva;
-        public int celkovaujetavzdalenost;
-        public int naklad;
+
 
         public string Jmeno { get; set; }
         public int Nosnost { get; set; }
         public int PalivoVNadrzi { get; set; }
         public int SpotrebaPaliva { get; set; }
         public int CelkovaUjetaVzdalenost { get; set; }
-        public int Naklad { get => naklad; }
-
+        public int Naklad { get; set; }
         public Nakladak()
         {
             Jmeno = " ";
@@ -26,16 +21,16 @@ namespace test.nakladak
             PalivoVNadrzi = 1000;
             SpotrebaPaliva = 35;
             CelkovaUjetaVzdalenost = 2000;
-            naklad = 0;
+            Naklad = 0;
 
         }
 
         public void Nalozit()
         {
-            naklad += 1000;
-            if (naklad > 25000)
+            Naklad += 1000;
+            if (Naklad > 25000)
             {
-                naklad = 25000;
+                Naklad = 25000;
             }
 
 
@@ -43,29 +38,29 @@ namespace test.nakladak
 
         public void Vylozit()
         {
-            naklad -= 1000;
-            if (naklad < 0)
+            Naklad -= 1000;
+            if (Naklad < 0)
             {
-                naklad = 0;
+                Naklad = 0;
             }
         }
 
         public void Natankovat()
         {
-            palivovnadrzi += 1000;
-            if(palivovnadrzi > 3000)
+            PalivoVNadrzi += 1000;
+            if(PalivoVNadrzi > 3000)
             {
-                palivovnadrzi = 3000;
+                PalivoVNadrzi = 3000;
             }
 
         }
 
         public void Jet()
         {
-            palivovnadrzi -= 500;
-            if (palivovnadrzi < 0)
+            PalivoVNadrzi -= SpotrebaPaliva;
+            if (PalivoVNadrzi < 0)
             {
-                palivovnadrzi = 0;
+                PalivoVNadrzi = 0;
             }
         }
 
